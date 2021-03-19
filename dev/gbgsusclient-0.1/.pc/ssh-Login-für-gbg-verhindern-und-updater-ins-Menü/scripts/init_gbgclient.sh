981a35b102820f647fff0c1b164b151407b34083
Felix Schoppe <felix.schoppe@gbg-seelze.eu>
2021-03-19 13:25:30 +0100

    ssh-Login für gbg im gbgsusclient verhindern und Updater ins Dock

--- dev/gbgsusclient-0.1/.pc/ssh-Login-für-gbg-verhindern-und-updater-ins-Menü/scripts/init_gbgclient.sh
+++ dev/gbgsusclient-0.1/.pc/ssh-Login-für-gbg-verhindern-und-updater-ins-Menü/scripts/init_gbgclient.sh
@@ -2,7 +2,7 @@
 #Dieses Skript setzt alle Verbindungen zurück und definiert das Aussehen des GBG-Desktops
 setxkbmap de
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/GBG_Custom_Background.png
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop','MountPrivateContainer.desktop','UMountPrivateContainer.desktop','firefox.desktop','libreoffice-writer.desktop','libreoffice-calc.desktop','libreoffice-impress.desktop','gnome-terminal.desktop','google-chrome.desktop','openboard.desktop','Write.desktop','xournal.desktop','updater.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop','MountPrivateContainer.desktop','UMountPrivateContainer.desktop','firefox.desktop','libreoffice-writer.desktop','libreoffice-calc.desktop','libreoffice-impress.desktop','gnome-terminal.desktop','google-chrome.desktop','openboard.desktop','Write.desktop','xournal.desktop']"
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 30
