1ff4421050a583b235b4a9b522e52c703a2c46f2
Felix Schoppe <felix.schoppe@gbg-seelze.eu>
2021-09-18 19:06:01 +0200

    Code für wpasupplicant-Fix aus updater.sh entfernt

--- dev/gbglehrerclient2-0.1/.pc/Code-für-wpasupplicant-Fix-entfernen/updater/updater.sh
+++ dev/gbglehrerclient2-0.1/.pc/Code-für-wpasupplicant-Fix-entfernen/updater/updater.sh
@@ -46,6 +46,19 @@ else
    dpkg -i /opt/klett-sprachen_2.2.0_amd64.deb
 fi
 
+#http://schoppemail.de/deb/wpasupplicant_2.9-1ubuntu4.1-PATCHED_amd64.deb
+#wpasupplicant fixen
+
+#if [ -f /opt/wpasupplicant_2.9-1ubuntu4.1-PATCHED_amd64.deb ]; then
+#   echo "wpa_supplicant schon gefixt."
+#else
+#   echo "--------------"
+#   echo "Fix von wpa_supplicant installieren."
+#   echo "--------------"
+#   wget http://schoppemail.de/deb/wpasupplicant_2.9-1ubuntu4.1-PATCHED_amd64.deb -O /opt/wpasupplicant_2.9-1ubuntu4.1-PATCHED_amd64.deb
+#   dpkg -i /opt/wpasupplicant_2.9-1ubuntu4.1-PATCHED_amd64.deb
+#fi
+
 #Prüfen, ob xournalpp vorhanden ist, falls ja,
 #entfernen und neu installieren
 
